ab17cdd4d13b75db48e27dd4ffe585240c0a73c6
Richard Bach <richard.bach@tabordasolutions.com>
2016-04-11 18:29:21 -0700

    help links to prod url now

--- modules/login/src/main/js/nics/modules/LoginModule.js
+++ modules/login/src/main/js/nics/modules/LoginModule.js
@@ -42,7 +42,7 @@ define([
                 html: "<div style=\"cursor: pointer;font-family: Arial,Verdana,sans-serif;font-size: 11px;font-weight: bold;color:blue\"><u>Help</u></div>",
                 listeners: {
                     click: function(){
-						window.open('https://scout.stg.tabordasolutions.net/scouthelp/', '_blank');
+						window.open('https://scout.ca.gov/scouthelp/', '_blank');
                     },
                     element: 'el'
                 }
--- modules/login/src/main/resources/META-INF/resources/login/loginFailed.jsp
+++ modules/login/src/main/resources/META-INF/resources/login/loginFailed.jsp
@@ -76,7 +76,7 @@
                         <a href="settings.html">Settings</a>
                     </span>
                     <span>
-                        <a href="https://scout.stg.tabordasolutions.net/scouthelp/" target="_blank">Help</a>
+                        <a href="https://scout.ca.gov/scouthelp/" target="_blank">Help</a>
                     </span>
                 </span>
             </div>
