b0b0ec0ada878f2ab1d3ef5f032e3bd3940d45f8
Sneha Nagole <snagole@bitbucket.org>
2017-05-16 08:54:26 -0700

    Temp changes for styling raws layer

--- modules/datalayer/src/main/js/nics/modules/datalayer/DatalayerBuilder.js
+++ modules/datalayer/src/main/js/nics/modules/datalayer/DatalayerBuilder.js
@@ -138,29 +138,12 @@ define(['iweb/CoreModule', 'ol', './TokenManager', './ArcGISFeatureRequestManage
 			vectorSource.set("deferReload", true);
 			
 			//create default style
-			var stroke = new ol.style.Stroke({
-	    	   color: '#3399CC',
-	    	   width: 1.25
-	    	 });
-			
-			var fill = new ol.style.Fill({
-				color: 'rgba(215, 40, 40, 0.9)'
-	        });
-			
-			var style = new ol.style.Style({
-    	     image: new ol.style.Circle({
-    	         fill: fill,
-    	         stroke: stroke,
-    	         radius: 5
-    	       }),
-	           fill: fill,
-    	       stroke: stroke
-    	     });
+			var style = this.buildWFSStyle(url, layername, config);
 			
 			return new ol.layer.Vector({
 				opacity: config.opacity || 1,
 				source: vectorSource,
-				style: style 
+				style: style
 		    });	
 		},
 		
@@ -355,6 +338,65 @@ define(['iweb/CoreModule', 'ol', './TokenManager', './ArcGISFeatureRequestManage
 			
 			return layer;
 			
-		}
-	});
+		},
+
+        buildWFSStyle:  function(url, layername, config) {
+            var style;
+            	if(layername != 'scout:raws_view') {
+            			    var stroke = new ol.style.Stroke({
+            	    	        color: '#3399CC',
+            	    	        width: 1.25
+            	    	    });
+
+            			    var fill = new ol.style.Fill({
+            				    color: 'rgba(215, 40, 40, 0.9)'
+            	            });
+
+            			    style = new ol.style.Style({
+                	            image: new ol.style.Circle({
+                	            fill: fill,
+                	            stroke: stroke,
+                	            radius: 5
+                	        }),
+            	            fill: fill,
+                	        stroke: stroke
+                	        });
+                	   } else {
+                	     style = function(feature, resolution) {
+                                                      var blackFill = new ol.style.Fill({color: 'black'});
+                                                      var stroke = new ol.style.Stroke({color: 'black', width: 0.3});
+                                                      if(feature.get('air_temperature')) {
+                                                          return new ol.style.Style({
+                                                             image: new ol.style.Circle({
+                                                                 fill: blackFill,
+                                                                 stroke: stroke,
+                                                                 radius: 3
+                                                             }),
+                                                             fill: blackFill,
+                                                             stroke: stroke,
+                                                             text: new ol.style.Text({
+                                                                 text: feature.get('air_temperature') + ' ℉',
+                                                                 scale: 1.3,
+                                                                 offsetX: -11,
+                                                                 offsetY:-11,
+                                                                 fill: new ol.style.Fill({
+                                                                     color: 'blue'
+                                                                 }),
+                                                                 stroke: stroke
+                                                             })
+                                                          });
+                                                      } else {
+                                                      return new ol.style.Style({
+                                                                 image: new ol.style.Circle({
+                                                                     fill: blackFill,
+                                                                     stroke: stroke,
+                                                                     radius: 3
+                                                                 }),
+                                                                 fill: blackFill,
+                                                                 stroke: stroke,
+                                                             });
+                                                      }
+                                                 };
+                	   }
+        }
 });
