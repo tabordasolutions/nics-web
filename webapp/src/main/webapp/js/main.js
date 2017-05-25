398565162bf60fec84b8dcdbfbe3637c38a57e92
Terry Weaver <terry.weaver@tabordasolutions.com>
2017-05-25 10:17:36 -0700

    Bumped version for next release.

--- webapp/src/main/webapp/js/main.js
+++ webapp/src/main/webapp/js/main.js
@@ -28,7 +28,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 require.config({
-    urlArgs: "v=2017.1.3"
+    urlArgs: "v=2017.1.4"
 });
 require([
     "iweb/CoreModule", "iweb/modules/MapModule",
