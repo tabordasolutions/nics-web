24c84938c69b44cd3a4c869dc810a8f79076b5b4
Terry Weaver <terry.weaver@tabordasolutions.com>
2017-07-18 11:07:11 -0700

    Changed Raws Feature Styler to use static png's instead of dynamically rendered SVGs. This fixes the feature in IE11.

--- modules/datalayer/src/main/js/nics/modules/datalayerstyle/RawsFeatureStyler.js
+++ modules/datalayer/src/main/js/nics/modules/datalayerstyle/RawsFeatureStyler.js
@@ -1,4 +1,33 @@
-define(["jquery", "ol", "./WindBarbSvg_Generator"], function($, ol, windbarbs){
+/*
+ * Copyright (c) 2008-2017, Taborda Solutions
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+define(["jquery", "ol"], function($, ol){
 
     var barbstyles = new Array();
 
@@ -10,13 +39,18 @@ define(["jquery", "ol", "./WindBarbSvg_Generator"], function($, ol, windbarbs){
         var wind_speed = feature.get('wind_speed');
         var air_temp = feature.get('air_temperature');
         var feature_id = feature.get('id');
-        if (wind_direction > 0 && wind_speed > 0) {
+        if (wind_direction >= 0 && wind_speed > 0) {
+            var barbSpeed = Math.round(wind_speed / 5) * 5;
+            if (barbSpeed > 150) {
+                barbSpeed = 150;
+            } //Max windbarb representation;
             var style = barbstyles[feature_id];
             if (style) {
                 return style;
             }
             else {
-                var windbarbsvg = windbarbs.GetWindArrowSvg(parseInt(wind_speed),parseFloat(wind_direction),40);
+                var arrowWidth = 40;
+                //var windbarbsvg = windbarbs.GetWindArrowSvg(parseInt(wind_speed),parseFloat(wind_direction),arrowWidth);
                 var featuretext;
                 if (air_temp) {
                     featuretext = new ol.style.Text({
@@ -31,8 +65,9 @@ define(["jquery", "ol", "./WindBarbSvg_Generator"], function($, ol, windbarbs){
                 barbstyles[feature_id] = new ol.style.Style({
                     image: new ol.style.Icon({
                         opacity: 1,
-                        src: 'data:image/svg+xml;utf8,' + encodeURIComponent(windbarbsvg),
-                        scale: 1
+                        src: 'images/windbarb/arrow' + barbSpeed + '.png',
+                        scale: 1,
+                        rotation:wind_direction
                     }),
                     text: featuretext
                 });
