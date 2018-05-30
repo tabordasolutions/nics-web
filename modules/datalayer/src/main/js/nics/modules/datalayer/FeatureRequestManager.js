010ea0a3c93d5df175ece7ae74d82d783c5f0183
snagole <sneha.nagole@tabordasolutions.com>
2018-05-30 12:16:50 -0700

    Minor change to FeatureRequestManager.js

--- modules/datalayer/src/main/js/nics/modules/datalayer/FeatureRequestManager.js
+++ modules/datalayer/src/main/js/nics/modules/datalayer/FeatureRequestManager.js
@@ -103,7 +103,7 @@ define(["ol",'ext', 'iweb/CoreModule','iweb/modules/MapModule'],
 
         function requestWMSFeatureInfo(layer, evt, container) {
             map = MapModule.getMap();
-            var infoFormat = layer.getSource().urls[0].includes('geoserver') ? 'application/json' : 'application/geojson';
+            var infoFormat = layer.getSource().urls[0].toLowerCase().includes('geoserver') ? 'application/json' : 'application/geojson';
             var url = layer.getSource().getGetFeatureInfoUrl(
                     evt.mapBrowserEvent.coordinate,
                     map.getView().getResolution(),
