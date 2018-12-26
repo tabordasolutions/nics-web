67d24f9df8e6a4d9d1b7fff20d67bf138727c962
snagole <sneha.nagole@tabordasolutions.com>
2018-12-26 10:54:42 -0800

    Changes to use requirejs optimizer for accountinfo module

---
+++ modules/accountinfo/src/main/js/accountinfo-rs-build.js
@@ -0,0 +1,12 @@
+({
+    findNestedDependencies: false,
+
+    paths: {
+        'ext': 'empty:',
+        'iweb/CoreModule': 'empty:',
+        'nics/modules/UserProfileModule': 'empty:',
+        'nics/modules/report/common/FormVTypes': 'empty:',
+    },
+    name: 'nics/modules/AccountInfoModule',
+    out: '../../../target/classes/META-INF/resources/js/nics/modules/accountinfo.js',
+})
