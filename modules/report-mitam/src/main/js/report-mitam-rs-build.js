6afb37b915db9c1fa87e40d5420949cf224f0fac
snagole <sneha.nagole@tabordasolutions.com>
2018-12-29 21:33:14 -0800

    Changes to add requirejs optimizer for report-mitam

---
+++ modules/report-mitam/src/main/js/report-mitam-rs-build.js
@@ -0,0 +1,12 @@
+({
+    paths: {
+        'ext': 'empty:',
+        'ol': 'empty:',
+        'iweb/CoreModule': 'empty:',
+        'nics/modules/UserProfileModule': 'empty:',
+//        'nics/modules/report/common/FormVTypes': 'empty:',
+    },
+
+    name: 'nics/modules/MitamReportModule',
+    out: '../../../target/classes/META-INF/resources/js/nics/modules/report-mitam.js',
+})
