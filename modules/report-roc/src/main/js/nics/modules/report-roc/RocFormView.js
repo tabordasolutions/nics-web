701e7ad0324cbb3a90e604e9b8214384080b943b
Nikhil Devre <nikhil.devre@tabordasolutions.com>
2019-07-06 14:35:43 -0700

    Conditionality Requirements: Resource Commitments changes based on ROC status.
    Conditionality Requirements: Resource Commitments need to change based on ROC status.

--- modules/report-roc/src/main/js/nics/modules/report-roc/RocFormView.js
+++ modules/report-roc/src/main/js/nics/modules/report-roc/RocFormView.js
@@ -594,22 +594,9 @@ function(Core, RocFormController, RocFormModel ) {
                                     { boxLabel: 'No divert on Air Tankers for life safety', name: 'resourcesAssigned', inputValue: 'No divert on Air Tankers for life safety', bind: {hidden: '{finalReport}'}},
                                     { boxLabel: 'Large Air Tanker (LAT) assigned', name: 'resourcesAssigned', inputValue: 'Large Air Tanker (LAT) assigned', bind: {hidden: '{finalReport}'}},
                                     { boxLabel: 'Continued commitment of CAL FIRE air and ground resources', name: 'resourcesAssigned', inputValue: 'Continued commitment of CAL FIRE air and ground resources', bind: {hidden: '{!updateReport}'}},
-                                    { boxLabel: 'All CAL FIRE air and ground resources released', name: 'resourcesAssigned', inputValue: 'All CAL FIRE air and ground resources released', bind: {hidden: '{!finalReport}' }},
-                                    { boxLabel: 'Other', name: 'other', inputValue: 'Other', reference: 'otherResourcesAssignedCheckboxRef'}
+                                    { boxLabel: 'All CAL FIRE air and ground resources released', name: 'resourcesAssigned', inputValue: 'All CAL FIRE air and ground resources released', bind: {hidden: '{!finalReport}' }}
                                 ]
-                            },
-                            {
-                                bind: {
-                                    value: '{otherResourcesAssigned}',
-                                    disabled: '{!otherResourcesAssignedCheckboxRef.checked}' },
-                                    fieldLabel: 'Other Resources Assigned',
-                                    vtype:'extendedalphanum',
-                                    validator: function(val) {
-                                        return (!this.disabled && !val) ? "Other Resources Assigned is required" : true;
-                                    }, listeners: { disable: function() {
-                                       this.reset();
-                                    }}, cls: 'roc-required'
-                            },
+                            }
                         ]
 
                    },
