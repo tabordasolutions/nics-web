f0477686ba28f9ebb678ac4bbbb056b96dc086fa
Nikhil Devre <nikhil.devre@tabordasolutions.com>
2020-02-05 09:54:39 -0800

    Generic message added for failure in onCreateIncident().
    Generic message added for failure in onCreateIncident().

--- modules/incident/src/main/js/nics/modules/incident/IncidentController.js
+++ modules/incident/src/main/js/nics/modules/incident/IncidentController.js
@@ -239,7 +239,7 @@ define(['iweb/CoreModule',
 			
 			onCreateIncident: function(evt, response){
 				if(response.message != "OK"){
-				    console.log(response);
+				    console.log("Create Incident Failed: ", response);
 					Ext.MessageBox.alert("Status", "Create Incident Failed. Please try again.");
 				}else{
 					//Reset Display
